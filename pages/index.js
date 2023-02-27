3b06c845c90ed550521cb36c424b4f61896e8c47
bediako95 <p.bediako1999@gmail.com>
2023-02-27 16:22:32 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import Layout from "@/components/layout";
 import EmployeeForm from "@/components/employees/NewEmployee/Form";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import EmployeeAction from "@/components/employees/NewEmployee/EmployeeActions";
 import { useRouter } from "next/router";
 
@@ -10,10 +10,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   //Toggle the form
-  const [visible, setVisible] = useState(false);
-  const handler = () => {
-    setVisible(!visible);
-  };
+  // const [visible, setVisible] = useState(false);
+  //const handler = () => {
+  // setVisible(!visible);
+  //};
   const router = useRouter();
 
   useEffect(() => {
