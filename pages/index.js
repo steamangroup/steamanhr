12e37e65920422d40f7e69c4cf80fc2eb0807734
bediako95 <p.bediako1999@gmail.com>
2023-03-09 19:15:04 +0000

    update home page

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,33 @@ import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import Layout from "@/components/layout";
 import EmployeeForm from "@/components/employees/NewEmployee/Form";
-import { useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import EmployeeAction from "@/components/employees/NewEmployee/EmployeeActions";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Box, Center, Text } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 
 export default function Home() {
-  //Toggle the form
-  const [visible, setVisible] = useState(false);
-  const handler = () => {
-    setVisible(!visible);
-  };
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/auth/login");
+    }, 500);
+  }, []);
+
   return (
-    <>
-      <Layout>
-        {/***** <EmployeeAction handleClick={handler} />* */}
-        {visible ? <EmployeeForm></EmployeeForm> : <></>}
-      </Layout>
-    </>
+    <Box bg="#089d8d" h={400}>
+      <Center>
+        <Text fontSize={60} mt="10rem" color="white" fontWeight="bold">
+          Welcome to{" "}
+          <span
+            style={{
+              color: "#d4af37",
+            }}
+          >
+            STEAMANHR
+          </span>
+        </Text>
+      </Center>
+    </Box>
   );
 }
